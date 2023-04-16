ce20a377d47fbb2fa86fd10efb0864f7c4a16d18
Dass33 <danieldvoraku@gmail.com>
2023-04-16 11:52:32 +0200

    Pridani zdi

--- BreakingWallGame/BreakingWallGame/Form1.Designer.cs
+++ BreakingWallGame/BreakingWallGame/Form1.Designer.cs
@@ -31,34 +31,66 @@
             this.components = new System.ComponentModel.Container();
             this.pbplatno = new System.Windows.Forms.PictureBox();
             this.tmrRedraw = new System.Windows.Forms.Timer(this.components);
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.tsMouseCor = new System.Windows.Forms.ToolStripStatusLabel();
+            this.tsPlayerCor = new System.Windows.Forms.ToolStripStatusLabel();
             ((System.ComponentModel.ISupportInitialize)(this.pbplatno)).BeginInit();
+            this.statusStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // pbplatno
             // 
-            this.pbplatno.Location = new System.Drawing.Point(0, 0);
+            this.pbplatno.Location = new System.Drawing.Point(2, 0);
             this.pbplatno.Name = "pbplatno";
             this.pbplatno.Size = new System.Drawing.Size(680, 600);
             this.pbplatno.TabIndex = 0;
             this.pbplatno.TabStop = false;
+            this.pbplatno.Click += new System.EventHandler(this.pbplatno_Click);
             // 
             // tmrRedraw
             // 
             this.tmrRedraw.Interval = 10;
             this.tmrRedraw.Tick += new System.EventHandler(this.tmrRedraw_Tick);
             // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsMouseCor,
+            this.tsPlayerCor});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 639);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(684, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // tsMouseCor
+            // 
+            this.tsMouseCor.Name = "tsMouseCor";
+            this.tsMouseCor.Size = new System.Drawing.Size(76, 17);
+            this.tsMouseCor.Text = "coordionates";
+            // 
+            // tsPlayerCor
+            // 
+            this.tsPlayerCor.Name = "tsPlayerCor";
+            this.tsPlayerCor.Size = new System.Drawing.Size(118, 17);
+            this.tsPlayerCor.Text = "toolStripStatusLabel1";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(684, 661);
+            this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.pbplatno);
             this.Name = "Form1";
             this.Text = "Breaking Wall";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
             ((System.ComponentModel.ISupportInitialize)(this.pbplatno)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -66,6 +98,9 @@
 
         private System.Windows.Forms.PictureBox pbplatno;
         private System.Windows.Forms.Timer tmrRedraw;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel tsMouseCor;
+        private System.Windows.Forms.ToolStripStatusLabel tsPlayerCor;
     }
 }
 
--- BreakingWallGame/BreakingWallGame/Form1.cs
+++ BreakingWallGame/BreakingWallGame/Form1.cs
@@ -19,20 +19,37 @@ namespace BreakingWallGame
         //kulicka
         int mintXKulicky, mintYKulicky;
         int mintPohybX, mintPohybY;
+
+        bool boolKulickaOdrazena;
+
+        const int mintRadiusKulicky = 24;
+        const float mfloatHeightOfKulicky = 1.3f;
         //player
         int mintXPlayer, mintYPlayer;
         int mintPohybXPlayer;
 
         bool boolMoveLeftPlayer;
-        bool boolKulickaOdrazena;
+
+        const int mintWidthPlayer = 120;
+        const int mintHeightPlayer = 25;
+        const int mintMomentumOdrazuPlayer = 3;
+        const float mfloatHeightOfPlayer = 1.2f;
+        //zed
+        int mintPocetBloku, mintMezeraStrany;
+        int mintXBloku = 0, mintYBloku = 0;
+
+        const int mintXmezeraBloku = 105; 
+        const int mintYmezeraBloku = 50;
+        const int mintPocetRadBloku = 4;
+        const int mintWidthBloku = 90;
+        const int mintHeightBloku = 30;
+
+        int[] marrZnicenebloky;
 
         //konstanty => nemeni se v prubehu programu
-        const int mintWidthPlayer = 100;
-        const int mintHeightPlayer = 20;
-        const float mfloatHeightOfPlayer = 1.2f;
-        const int mintRadiusKulicky = 23;
-        const int mintRychlostPosunu = 5;
+        const int mintRychlostPosunu = 7;
         const int tmrRedrawSpeed = 20;
+        const int mintMarginOfError = 15;
 
         private void Form1_Load(object sender, EventArgs e)
         {
@@ -40,14 +57,20 @@ namespace BreakingWallGame
             tmrRedraw.Interval = tmrRedrawSpeed;
             tmrRedraw.Start();
         }
+        
+        private void pbplatno_Click(object sender, EventArgs e)
+        {
+
+        }
 
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
-            if (e.KeyCode == Keys.Left)
+            //po zmacknuti mezerniku meni smer kterym se pohybuje hrac
+            if (e.KeyCode == Keys.Space && boolMoveLeftPlayer == false)
             {
                 boolMoveLeftPlayer = true;
             }
-            if (e.KeyCode == Keys.Right)
+            else if (e.KeyCode == Keys.Space)
             {
                 boolMoveLeftPlayer = false;
             }
@@ -57,7 +80,6 @@ namespace BreakingWallGame
         {
             InitializeComponent();
             InitPromenych();
-            
         }
 
         private void InitPromenych()
@@ -67,54 +89,110 @@ namespace BreakingWallGame
 
             //init promenych kulicky
             mintXKulicky = pbplatno.Width / 2;
-            mintYKulicky = pbplatno.Height / 2;
+            mintYKulicky = (int)(pbplatno.Height / mfloatHeightOfKulicky);
             mintPohybX = mintRychlostPosunu;
             mintPohybY = mintRychlostPosunu;
             mintYPlayer = (int)(pbplatno.Height / mfloatHeightOfPlayer);
 
             //init promenych hrace
             mintXPlayer = pbplatno.Width / 2;
-            mintPohybXPlayer = mintRychlostPosunu;
+            mintPohybXPlayer = (int)(mintRychlostPosunu * 1.4);
 
-            //init Postaveni zdi
-
-            //spocitani kolik bloku zdi se ma vzkreslit a jak velke mezerz maji byt po stranach 
-            //vydeli sirku pbplatno velikosti jednotlivych bloku (cihel) + mezer mezi nimi
-            //ke zbztku pricte velikost jedne mezery a ten nasledne rozdeli na dva dili => mezery od okarju
-            //if statmenty kontroluji jestli neni prave vykreslovany blok prvni nebo posledni 
-
-            //for loop ktery vykresluje nove bloky v tolika radach kolik je zadano
+            //spocitani kolik bloku zdi se ma vzkreslit a jak velke mezery maji byt po stranach
+            mintPocetBloku = pbplatno.Width / mintXmezeraBloku;
+            //urceni delky array, tak aby v ni bylo misto pro vsechny bloky
+            //vymeneni defaultni nuly za -1, aby hra nezacala s chybejcim prvnim dilkem
+            marrZnicenebloky = Enumerable.Repeat(-1, mintPocetRadBloku * mintPocetBloku).ToArray();
+            // zjisti zbztek deleni sirky pbplatno velikosti mezer bloku (cihel)
+            //pricte velikost rozdilu jedne mezery a velikosti bloku ten nasledne rozdeli na dva dili => mezery od okarju
+            mintMezeraStrany = ((pbplatno.Width % mintXmezeraBloku) / 2) + (mintXmezeraBloku - mintWidthBloku) / 2;
         }
 
         //kontroluje jestli kulicka prave nenarazi do nektereho z objektu (Zed), nebo hrace
         public void DetekceNarazuKulicky(int mintX, int mintY, int mintWidthObject, int mintHeightObject)
         {
             if ((mintYKulicky + mintRadiusKulicky >= mintY) &&
-                (mintYKulicky + mintRadiusKulicky <= mintY + mintHeightObject) &&
-                (mintXKulicky >= mintX) && (mintXKulicky <= mintX + mintWidthObject))
+                (mintYKulicky + mintRadiusKulicky <= mintY + mintMarginOfError) &&
+                (mintXKulicky + mintRadiusKulicky >= mintX) && 
+                (mintXKulicky <= mintX + mintWidthObject))
             {
+                //odrzeni kulicky
+                mintPohybY = mintPohybY * (-1);
                 boolKulickaOdrazena = true;
+                
+                //pokud odrzeno hracem ziska kulicka cast momenta hrace
+                if (mintY == mintYPlayer && boolMoveLeftPlayer)
+                {
+                    mintPohybX= mintPohybX + (mintPohybXPlayer / mintMomentumOdrazuPlayer) * (-1);
+                    mintPohybY = mintPohybY + 1/ mintMomentumOdrazuPlayer * mintMomentumOdrazuPlayer;
+                }
+                else if (mintY == mintYPlayer && boolMoveLeftPlayer == false)
+                {
+                    mintPohybX = mintPohybX + (mintPohybXPlayer / mintMomentumOdrazuPlayer);
+                    mintPohybY = mintPohybY + 1 / mintMomentumOdrazuPlayer * mintMomentumOdrazuPlayer;
+                }
             }
             else boolKulickaOdrazena = false;
         }
-            private void tmrRedraw_Tick(object sender, EventArgs e)
+        private void tmrRedraw_Tick(object sender, EventArgs e)
         {
-            //smazat scenu
+            //for development purpuses delete awfter testing finished
+            tsMouseCor.Text = "Mouse X: " + MousePosition.X + " Y: " + MousePosition.Y;
+            tsPlayerCor.Text = "Player X: " + mintXPlayer + " Y: " + mintYPlayer;
+
+            //smazani sceny
             mobjGrafika.Clear(Color.White);
 
+            //for loop ktery vykresluje nove bloky v tolika radach kolik je zadano
+            mintYBloku += mintYmezeraBloku;
+            for (int i = 0, a = 0; i < mintPocetRadBloku * mintPocetBloku; i++, a++)
+            {
+                if (a == mintPocetBloku)
+                {
+                    //vykresleni dalsi rady posunute dolu
+                    mintYBloku += mintYmezeraBloku;
+                    a = 0;
+                }
+
+                //prvni blok veliost mintMezeraStrany se pouzije misto mintXmezera
+                if (a == 0)
+                {
+                    mintXBloku = mintMezeraStrany;
+                }
+                else mintXBloku += mintXmezeraBloku;
+                                             
+                if (marrZnicenebloky == null || (!Array.Exists(marrZnicenebloky, element => element == a + i * mintPocetBloku)))
+                {
+                    mobjGrafika.FillRectangle(Brushes.DarkBlue, mintXBloku, mintYBloku, mintWidthBloku, mintHeightBloku);
+                    //pri detekovani narazu kulicky zvisujeme hodnotu Y tak aby hitbox odpovida stredu bloku
+                    DetekceNarazuKulicky(mintXBloku, mintYBloku + mintHeightBloku, mintWidthBloku, mintHeightBloku);
+
+                    if (boolKulickaOdrazena)
+                    {
+                        //ulozeni souradnic jednotlivizch ynicenzch bloku
+                        //a udave x souradnici a i * mintPocetBloku udava y souradnici
+                        marrZnicenebloky = marrZnicenebloky.Append(a + i * mintPocetBloku).ToArray();
+                    }
+                }            
+            }
+            mintXBloku = mintYBloku = 0;
+        
             //vykresleni kulicky
             mobjGrafika.FillEllipse(Brushes.Red, mintXKulicky, mintYKulicky, mintRadiusKulicky, mintRadiusKulicky);
-
             //posun kulicky
             mintXKulicky += mintPohybX;
             mintYKulicky += mintPohybY;
 
             //detekce jestli kulicka narazi na hrace
+           
             DetekceNarazuKulicky(mintXPlayer, mintYPlayer, mintWidthPlayer, mintHeightPlayer);
+                
+            
 
             //pokud kulicka narazi do steny, stropu nebo hrace odrazi se opacnym smerem => *(-1)
-            if (mintYKulicky < 0 || boolKulickaOdrazena) //(mintYKulicky + mintRadiusKulicky) > pbplatno.Height || //Vratit nazpatek pokud se kulicka odrayi od spodni hrany)
+            if (mintYKulicky < 0) //odrzeni od spodni hrany: || (mintYKulicky + mintRadiusKulicky) > pbplatno.Height
             {
+                mintYKulicky= 0;
                 mintPohybY = mintPohybY * (-1);
             }
             if ((mintXKulicky + mintRadiusKulicky) > pbplatno.Width || mintXKulicky < 0)
@@ -125,19 +203,20 @@ namespace BreakingWallGame
             //vykresleni odrazeci plochy
             mobjGrafika.FillRectangle(Brushes.DarkGray, mintXPlayer, mintYPlayer, mintWidthPlayer, mintHeightPlayer);
 
-            //posun hrace
-            if (boolMoveLeftPlayer && mintXPlayer > 0)
+            //posun hrace pokud je v rozmezi platna
+            if (mintXPlayer >= 0 && boolMoveLeftPlayer)
             {
                 mintXPlayer -= mintPohybXPlayer;
             }
-            if (boolMoveLeftPlayer == false && mintXPlayer < (pbplatno.Width - mintWidthPlayer))
+            else if (mintXPlayer < (pbplatno.Width - mintWidthPlayer) && boolMoveLeftPlayer == false)
             {
                 mintXPlayer += mintPohybXPlayer;
             }
 
             //konec hry
-            if(mintYKulicky > pbplatno.Height)
+            if (mintYKulicky > pbplatno.Height)
             {
+                
                 InitPromenych();
             }
         }
